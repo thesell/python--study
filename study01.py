ce3780641f318e92dc902439e885c0fd9f9490c0
jayboni <thesell372@gmail.com>
2022-10-21 17:57:16 +0900

    파이썬 표준포멧

--- study01.py
+++ study01.py
@@ -1,6 +1,59 @@
-name = "jaybonl"
+my_dict = {"name" : "jaybon", age : 12}
 
-print(name)
 
-# git
-# 터미널
+
+
+
+# my_set ={1,2,3,4,1,1,1,}
+# my_set = set ({[1,2,3,4,1,1,1,1]})
+# print(True or True)
+
+
+
+
+
+
+
+# def sun_two_num(n1, n2):
+#     return n1+ n2
+
+#  sum_num   sun_two_num(1,2)
+#  sum_num = 3
+ 
+#  print(sum_num)
+
+
+
+
+
+# def check_data():
+#     data = int(input())
+
+#     if data == 1 : 
+#         print("1이다")
+#     elif data == 2:
+#         print("2이다")
+#     elif data == 3:
+#         print("3이다")
+#     elif data == 4:
+#         print("4이다")        
+#     else:
+#         prine("모르겠다")
+        
+# check_data()
+# check_data()    
+# check_data()
+
+# x = 1
+# while True:
+#     print("가나다")
+#     if x == 10:
+#         break
+#     else:
+#         x = x + 1
+# for item in [1,2,3,4]:
+#     print(item)
+
+
+
+    
